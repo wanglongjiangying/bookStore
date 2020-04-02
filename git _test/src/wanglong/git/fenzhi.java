73c2bb523b36ff30f87d16f60917e3cf4bae09b4
wanglong <1959459788@qq.com>
2020-04-02 21:33:45 +0800

    创建一个dev1分支，并且添加了一个fenzhi类

---
+++ git _test/src/wanglong/git/fenzhi.java
@@ -0,0 +1,11 @@
+package wanglong.git;
+
+public class fenzhi {
+
+    public void fenzhi(){
+        System.out.println("分支的测试");
+    }
+    public void fenzhi2(){
+        System.out.println("dev2分支的测试");
+    }
+}
